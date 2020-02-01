7d359fede9a1dfa42b22cafeb7347ec6e42c9d4d
Antonina Povkh <apovkh@gmail.com>
2020-02-01 15:24:15 +0100

    Function checking if parentheses are balanced

---
+++ BalanceCheck.py
@@ -0,0 +1,10 @@
+def balance_check(s):
+    chars = []
+    matches = {')':'(',']':'[','}':'{'}
+    for c in s:
+        if c in matches:
+            if chars.pop() != matches[c]:
+                return False
+        else:
+            chars.append(c)
+    return chars == []        
